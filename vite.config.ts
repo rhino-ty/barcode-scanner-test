d377150ab49ac3537ee3b29e4624f49d68d77b16
rhino-ty <okko8522@gmail.com>
2025-07-07 02:07:55 +0900

    chore: 모든 의존성을 서버 번들에 포함 옵션 true
    Windows Server 환경에서의 호환성 문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// NODE_ENV=production npm run build:clean로 빌드 해야함
+// 윈도우(Powershell): $env:NODE_ENV="production"; npm run build
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  ssr: {
+    // 모든 의존성을 서버 번들에 포함 => Windows Server 환경에서의 호환성 문제 해결
+    noExternal: process.env.NODE_ENV === 'production' ? true : ['@ericblade/quagga2'],
+  },
 });
